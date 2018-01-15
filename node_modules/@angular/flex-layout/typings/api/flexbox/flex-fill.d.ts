6da25aade8dc11c26fe752328cfa0eeb8d4cf5cd
Guttz <gustavopguto@gmail.com>
2018-01-14 23:29:07 -0200

    Node_modules removed

--- node_modules/@angular/flex-layout/typings/api/flexbox/flex-fill.d.ts
+++
@@ -1,8 +0,0 @@
-import { ElementRef, Renderer2 } from '@angular/core';
-import { MediaMonitor } from '../../media-query/media-monitor';
-import { BaseFxDirective } from '../core/base';
-export declare class FlexFillDirective extends BaseFxDirective {
-    elRef: ElementRef;
-    renderer: Renderer2;
-    constructor(monitor: MediaMonitor, elRef: ElementRef, renderer: Renderer2);
-}
