cc5bc89bd127a59ad63e1e5921656159b9b22dcf
Guttz <gustavopguto@gmail.com>
2018-02-17 11:36:24 -0200

    Added header type: content type

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 	
     // Request headers you wish to allow
-    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
