3c411763f91c9947f738c4192bac5c55ccdb4d4f
Guttz <gustavopguto@gmail.com>
2018-04-22 13:16:54 -0300

    Small change just to maintain pattern

--- src/app/pages/acompanhamento/acompanhamento.component.ts
+++ src/app/pages/acompanhamento/acompanhamento.component.ts
@@ -138,7 +138,7 @@ export class AtendimentoComponent implements OnInit
   options = { day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit',
              minute: '2-digit', second: '2-digit' };
 
-  config = {linkLogo: "", termosOS: ""};
+  config = {linkLogo: "", termosOS: "", termoOSFinalizada: ""};
   
   constructor(public dialogRef: MatDialogRef<ClienteAtenderComponent>, 
     @Inject(MAT_DIALOG_DATA) public data: any,
